09de4c4d078e7096c7c99b06601cb66b392b87a5
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-10-19 20:57:58 +0500

    Сделала нарезку компонентов для страницы main.

---
+++ src/components/app/app.tsx
@@ -0,0 +1,9 @@
+import MainPage from '../../pages/main/main';
+
+function App(): JSX.Element {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,35 @@
+import Header from '../../components/header/header';
+import Tabs from '../../components/tabs/tabs';
+import PlacesSortingForm from './places-sorting-form';
+import PlaceCard from './place-card';
+import Map from './map';
+
+function MainPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <Header />
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <Tabs />
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">312 places to stay in Amsterdam</b>
+              <PlacesSortingForm />
+              <div className="cities__places-list places__list tabs__content">
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+              </div>
+            </section>
+            <Map />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default MainPage;
