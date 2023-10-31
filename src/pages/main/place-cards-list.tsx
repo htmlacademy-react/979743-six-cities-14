915a7d2eef70b3ce45e5c0c5ed009e61816ce0a9
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-10-31 19:21:48 +0500

    Добавила состояние в PlaceCardList. Добавила обработчик события onMouseOver для каждого article (обертка PlaceCard). Обработчик переопределяет состояние = id карточки, на которую наведена мышь.

--- src/pages/main/place-cards-list.tsx
+++ src/pages/main/place-cards-list.tsx
@@ -1,16 +1,26 @@
 import { OfferInfoProps } from '../offer/offer';
 import PlaceCard from './place-card';
+import { useState } from 'react';
 
 type PlaceCardsListProps = {
   offers: OfferInfoProps[];
 };
 
 function PlaceCardsList({offers}: PlaceCardsListProps): JSX.Element {
+  const [state, setState] = useState({
+    activeCardId: offers[0].id});
   return (
     <div className="cities__places-list places__list tabs__content">
       {offers.map((offer) => (
-        <article key = {offer.id} className="cities__card place-card">
-          <PlaceCard offer = {offer}/>
+        <article
+          key = {offer.id}
+          className="cities__card place-card"
+          onMouseOver = {() => setState({
+            ...state,
+            activeCardId: offer.id
+          })}
+        >
+          <PlaceCard offer = {offer} />
         </article>
       ))}
     </div>
