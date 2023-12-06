7f51b95dc7676e19e1d741a7580910a3bb7046b7
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-12-06 19:08:49 +0500

    Сделала в Header переход на /favorites по клику на e-mail пользователя

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -27,12 +27,12 @@ function Header(): JSX.Element {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link to="/favorites" className="header__nav-link header__nav-link--profile">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">{userInfo.email}</span>
                     <span className="header__favorite-count">3</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
                   <a className="header__nav-link" onClick={handleLogoutClick}>
